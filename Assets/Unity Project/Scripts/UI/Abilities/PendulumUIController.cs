9e818c159cfe1e79fa3bf94d1e215e2ace88ac71
NoahCedeno <noahcedeno1122@gmail.com>
2023-05-04 09:47:34 -0400

    DEV - Implemented Basic Pendulum UI
    Swings back, and forth, and back, and forth. Pretty cool!
    
    :llama:

---
+++ Assets/Unity Project/Scripts/UI/Abilities/PendulumUIController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumUIController : MonoBehaviour
+{
+    private bool StartsFromRight = true;
+    public bool IsAnimating { get; private set; }   
+    public float SwingAngleOffset = 45f;
+    public float VisibleAlpha = 0.85f;
+
+    private IEnumerator m_PendulumAnimationCRT;
+
+    private RectTransform m_PendulumTf;
+    private CanvasGroup m_CanvasGroup;
+
+    private void Start()
+    {
+        m_PendulumTf = transform.GetChild(0).GetComponent<RectTransform>();
+        m_CanvasGroup = GetComponent<CanvasGroup>();
+
+        // Initialize
+        m_CanvasGroup.alpha = 0f;
+    }
+
+    private void OnValidate()
+    {
+        if (m_PendulumTf == null)
+        {
+            m_PendulumTf = transform.GetChild(0).GetComponent<RectTransform>();
+        }
+    }
+
+    // + + + + | Functions | + + + +
+
+    public void ToggleSwing(float animationTime)
+    {
+        // Clear / Stop CRT if already running...
+        if (m_PendulumAnimationCRT != null)
+        {
+            StopCoroutine(m_PendulumAnimationCRT);
+            m_PendulumAnimationCRT = null;
+        }
+
+        // Start the CRT
+        StartsFromRight = !StartsFromRight;
+        m_PendulumAnimationCRT = PendulumSwingCRT(animationTime);
+        StartCoroutine(m_PendulumAnimationCRT);
+    }
+
+    private IEnumerator PendulumSwingCRT(float animationTime)
+    {
+        // Initialize
+        IsAnimating = true;
+        //m_CanvasGroup.alpha = 0.3f;
+        float currRotationValue = SwingAngleOffset * (StartsFromRight ? 1f : -1f);
+        float targetRotationValue = SwingAngleOffset * (StartsFromRight ? -1f : 1f);
+
+        for (float timeHelper = 0f; timeHelper < animationTime; timeHelper += Time.deltaTime)
+        {
+            m_PendulumTf.rotation = Quaternion.Euler(Vector3.forward * Mathf.SmoothStep(currRotationValue, targetRotationValue, timeHelper / animationTime));
+
+            // TODO: MUST be a better way to do this...
+            if (timeHelper < animationTime / 2f)
+            {
+                m_CanvasGroup.alpha = Mathf.SmoothStep(0f, VisibleAlpha, timeHelper / (animationTime / 4f));
+            }
+            else
+            {
+                m_CanvasGroup.alpha = Mathf.SmoothStep(VisibleAlpha, 0f, timeHelper / (animationTime));
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        // Terminate
+        IsAnimating = false;
+        m_CanvasGroup.alpha = 0f;
+        StopCoroutine(m_PendulumAnimationCRT);
+    }
+}
